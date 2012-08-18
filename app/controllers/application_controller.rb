105b4c3e60d1d8647c5d9efc91bb22b4d6f7c6f1
Albert Pai <iamalbertpai@gmail.com>
2012-08-17 18:10:24 -0700

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
  protect_from_forgery
-before_filter :set_access
+after_filter :set_access
 
 def set_access
   @response.headers["Access-Control-Allow-Origin"] = "*"
